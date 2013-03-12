a466b68ace4925d781d2c771f6e7ac9d502ed3cb
Dennis Calazans <contato@denniscalazans.com>
2013-03-12 20:46:06 -0300

    Novos arquivos CSS (position e display)

---
+++ css/old/position/galeria.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+	body {
+		margin: 0;
+	}
+		
+		#fotos {
+			background-color: #ccc;
+			padding: 10px;
+			width:620px;
+			text-align: center;
+			margin-left: auto;
+			position: relative;
+			margin-right: auto;
+		}
+
+		.foto {
+			background-color: #efefef;
+			width: 100px; 
+			height: 100px;
+			display: inline-block;
+			margin: 10px;
+			line-height: 100px;
+		}
+
+		.foto:nth-child(even) {
+			background-color: #ddd;
+		}
+	
+
+		#foto7 {
+			position: relative;
+			top: 30px;
+			left: 30px;
+		}
+
+		#foto9 {
+			position: relative;
+			bottom: 30px;
+			right: 30px;
+		}
+
+		#foto14 {
+			position: absolute;
+			bottom: 0;
+			right: 0;
+		}
+	</style>
+</head>
+<body>
+
+	<div id="fotos">
+		<div id="foto1" class="foto">Foto 1</div>
+		<div id="foto2" class="foto">Foto 2</div>
+		<div id="foto3" class="foto">Foto 3</div>
+		<div id="foto4" class="foto">Foto 4</div>
+		<div id="foto5" class="foto">Foto 5</div>
+		<div id="foto6" class="foto">Foto 6</div>
+		<div id="foto7" class="foto">Relativo</div>
+		<div id="foto8" class="foto">Foto 8</div>
+		<div id="foto9" class="foto">Relativo</div>
+		<div id="foto10" class="foto">Foto 10</div>
+		<div id="foto11" class="foto">Foto 11</div>
+		<div id="foto12" class="foto">Foto 12</div>
+		<div id="foto13" class="foto">Foto 13</div>
+		<div id="foto14" class="foto">Foto 14</div>
+		<div id="foto15" class="foto">Foto 15</div>
+		<div id="foto16" class="foto">Foto 16</div>
+		<div id="foto17" class="foto">Foto 17</div>
+		<div id="foto18" class="foto">Foto 18</div>
+		<div id="foto19" class="foto">Foto 19</div>
+		<div id="foto20" class="foto">Foto 20</div>
+		<div id="foto21" class="foto">Foto 21</div>
+		<div id="foto22" class="foto">Foto 22</div>
+		<div id="foto23" class="foto">Foto 23</div>
+		<div id="foto24" class="foto">Foto 24</div>
+		<div id="foto25" class="foto">Foto 25</div>
+		<div id="foto26" class="foto">Foto 26</div>
+		<div id="foto27" class="foto">Foto 27</div>
+		<div id="foto28" class="foto">Foto 28</div>
+		<div id="foto29" class="foto">Foto 29</div>
+		<div id="foto30" class="foto">Foto 30</div>
+		<div id="foto31" class="foto">Foto 31</div>
+		<div id="foto32" class="foto">Foto 32</div>
+		<div id="foto33" class="foto">Foto 33</div>
+		<div id="foto34" class="foto">Foto 34</div>
+		<div id="foto35" class="foto">Foto 35</div>
+		<div id="foto36" class="foto">Foto 36</div>
+		<div id="foto37" class="foto">Foto 37</div>
+		<div id="foto38" class="foto">Foto 38</div>
+		<div id="foto39" class="foto">Foto 39</div>
+		<div id="foto40" class="foto">Foto 40</div>
+		<div id="foto41" class="foto">Foto 41</div>
+		<div id="foto42" class="foto">Foto 42</div>
+		<div id="foto43" class="foto">Foto 43</div>
+		<div id="foto44" class="foto">Foto 44</div>
+		<div id="foto45" class="foto">Foto 45</div>
+		<div id="foto46" class="foto">Foto 46</div>
+		<div id="foto47" class="foto">Foto 47</div>
+		<div id="foto48" class="foto">Foto 48</div>
+		<div id="foto49" class="foto">Foto 49</div>
+		<div id="foto50" class="foto">Foto 50</div>
+	</div>
+</body>
+</html>
--- labs/rm/script.js
+++ labs/rm/script.js
@@ -1,21 +1,23 @@
-var APP = {
+window.APPNotasRM = {
 	_frame: window.frames.CLMain.document,
 
 	iniciar: function() {
-		jQuery(APP._frame).find("#dgAlunos tr").css('cursor', 'pointer')
+		$(window.APPNotasRM._frame).find("#dgAlunos tr").css('cursor', 'pointer')
 			.toggle(function(event) {
-				if(event.target.tagName != 'INPUT') 
-					jQuery(this).addClass('alunoSelecionado').css('background-color', '#fcc'); 
+				if(event.target.tagName != 'INPUT') {
+					$(event.currentTarget).addClass('alunoSelecionado').css('background-color', '#fcc'); 
+				}
 			}, function(event) { 
-				if(event.target.tagName != 'INPUT')
-					jQuery(this).removeClass('alunoSelecionado').css('background-color', '') 
+				if(event.target.tagName != 'INPUT') {
+					$(event.currentTarget).removeClass('alunoSelecionado').css('background-color', '');
+				}
 			});
 	},
 
 	aplicarATodos: function() {
 		var nota = prompt('Digite a nota');
 		if(!nota) return false;
-		jQuery(window.frames.CLMain.document)
+		$(window.frames.CLMain.document)
 			.find('input[type=text][name$=Nota]')
 			.val(nota);
 	
@@ -25,17 +27,12 @@ var APP = {
 		var nota = prompt('Digite a nota');
 		if(!nota) return false;
 
-		jQuery(window.frames.CLMain.document)
+		$(window.frames.CLMain.document)
 			.find("#dgAlunos tr.alunoSelecionado")
 			.find("input[type=text][name$=Nota]")
 			.val(nota)
 			.end().removeClass('alunoSelecionado');
 	}
 };
-var element1 = document.createElement("script");
-element1.src = "//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js";
-element1.type="text/javascript";
-element1.onload = function() {
-	APP.iniciar();
-}
-document.getElementsByTagName("head")[0].appendChild(element1);
+window.APPNotasRM.iniciar();
+alert('Notas RM instalado com sucesso!');
