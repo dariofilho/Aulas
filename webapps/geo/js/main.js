7a40662ebcdbb132154d501f1f42a9d4a932359f
Dennis Calazans <contato@denniscalazans.com>
2013-09-21 10:44:40 -0300

    WebApp GEO

---
+++ webapps/geo/js/main.js
@@ -0,0 +1,18 @@
+$("#entrada")
+
+	.on("blur", function(event){
+		var entrada = $(this).val(); 
+		var resultado = entrada.split("").reverse().join("");
+
+		$("#resultado").val(resultado).select();
+
+		localStorage.setItem('entrada', entrada);
+		localStorage.setItem('resultado', resultado);
+	})
+	.val(localStorage.getItem('entrada'));
+
+$("#resultado")
+	.val(localStorage.getItem('resultado'));
+
+
+
